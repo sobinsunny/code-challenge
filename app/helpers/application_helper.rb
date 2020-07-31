3bcdf9f23e8799f83ae07a122347508058cfd70a
Sobin <sobisunny@gmail.com>
2020-07-31 22:26:56 +0530

    Error helper for model validation error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,20 @@
 module ApplicationHelper
-
   def bootstrap_class_for(flash_type)
     {
       success: "alert-success",
       error: "alert-danger",
       alert: "alert-warning",
-      notice: "alert-info"
+      notice: "alert-info",
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
-  
+
+  def error_for(object)
+    if object.errors.any?
+      content_tag(:ul) do
+        object.errors.full_messages.each do |msg|
+          concat content_tag(:li, msg, style: "color: red")
+        end
+      end
+    end
+  end
 end
